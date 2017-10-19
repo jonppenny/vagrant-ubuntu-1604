b0e64c07ac51a69b986c5e5b89ca449f4ebb6e87
Jonathan Penny <jonppenny@users.noreply.github.com>
2017-10-19 16:59:54 +0100

    Config updates
    Updates to DocumentRoot and set folder permissions.

--- Vagrantfile
+++ Vagrantfile
@@ -3,5 +3,5 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, guest: 80, host: 8888
-  config.vm.synced_folder "public/", "/var/www/html"
+  config.vm.synced_folder "", "/var/www/html", :owner=> 'www-data', :group=>'www-data', :mount_options => ['dmode=775', 'fmode=775']
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,22 +8,22 @@ mysql_config_file="/etc/mysql/my.cnf"
 #default_apache_index="/var/www/html/index.html"
 
 update_go() {
-	# Update the server
-	apt-get update
-	apt-get -y upgrade
+    # Update the server
+    apt-get update
+    apt-get -y upgrade
 }
 
 apache_go() {
-	# Install Apache
-	apt-get -y install apache2
+    # Install Apache
+    apt-get -y install apache2
 
-	sed -i "s/^\(.*\)www-data/\1www-data/g" ${apache_config_file}
-	chown -R www-data:www-data /var/log/apache2
+    sed -i "s/^\(.*\)www-data/\1www-data/g" ${apache_config_file}
+    chown -R www-data:www-data /var/log/apache2
 
-	cat << EOF > ${apache_vhost_file}
+    cat << EOF > ${apache_vhost_file}
 <VirtualHost *:80>
         ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/html
+        DocumentRoot /var/www/html/public
         LogLevel debug
         ErrorLog /var/log/apache2/error.log
         CustomLog /var/log/apache2/access.log combined
@@ -34,56 +34,56 @@ apache_go() {
 </VirtualHost>
 EOF
 
-	a2dissite 000-default
-	a2ensite vagrant_vhost
+    a2dissite 000-default
+    a2ensite vagrant_vhost
 
-	a2enmod rewrite
+    a2enmod rewrite
 
-	service apache2 reload
-	update-rc.d apache2 enable
+    service apache2 reload
+    update-rc.d apache2 enable
 }
 
 php_go() {
-	sudo add-apt-repository -y ppa:ondrej/php
-	sudo apt-get -y update
-	sudo apt-get -y install php7.1 php7.1-common
-	sudo apt-get -y install php7.1-curl php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-mbstring
-	service apache2 reload
+    sudo add-apt-repository -y ppa:ondrej/php
+    sudo apt-get -y update
+    sudo apt-get -y install php7.1 php7.1-common
+    sudo apt-get -y install php7.1-curl php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-mbstring
+    service apache2 reload
 }
 
 mysql_go() {
-	# Install MySQL
-	echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
-	echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-	apt-get -y install mysql-client mysql-server
+    # Install MySQL
+    echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+    apt-get -y install mysql-client mysql-server
 
-	sed -i "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" ${mysql_config_file}
+    sed -i "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" ${mysql_config_file}
 
-	# Allow root access from any host
-	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION" | mysql -u root --password=root
-	echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql -u root --password=root
+    # Allow root access from any host
+    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION" | mysql -u root --password=root
+    echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql -u root --password=root
 
-	# Create database
-	echo "create database wordpress" | mysql -u root --password=root
+    # Create database
+    echo "create database wordpress" | mysql -u root --password=root
 
-	# Create user
-	echo "create user 'wordpress'@'localhost' identified by 'password'" | mysql -u root --password=root
+    # Create user
+    echo "create user 'wordpress'@'localhost' identified by 'password'" | mysql -u root --password=root
 
-	# Grant privileges
-	echo "grant all privileges on wordpress.* to 'wordpress'@'localhost'" | mysql -u root --password=root
+    # Grant privileges
+    echo "grant all privileges on wordpress.* to 'wordpress'@'localhost'" | mysql -u root --password=root
 
-	# Flush
-	echo "flush privileges" | mysql -u root --password=root
+    # Flush
+    echo "flush privileges" | mysql -u root --password=root
 
-	service mysql restart
-	update-rc.d apache2 enable
+    service mysql restart
+    update-rc.d apache2 enable
 }
 
 main() {
-	update_go
+    update_go
 
-	if [[ -e /var/lock/vagrant-provision ]]; then
-	    cat 1>&2 << EOD
+    if [[ -e /var/lock/vagrant-provision ]]; then
+	cat 1>&2 << EOD
 ################################################################################
 # To re-run full provisioning, delete /var/lock/vagrant-provision and run
 #
@@ -92,14 +92,14 @@ main() {
 # From the host machine
 ################################################################################
 EOD
-	    exit
-	fi
+	exit
+    fi
 
-	apache_go
-	php_go
-	mysql_go
+    apache_go
+    php_go
+    mysql_go
 
-	touch /var/lock/vagrant-provision
+    touch /var/lock/vagrant-provision
 }
 
 main
